bf49fa687121f8a1894bd8948a4975ed61e7c229
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-09-08 18:12:21 -0500

    Ejercicio 2

--- Semana 2/prueba.cpp
+++ Semana 2/prueba.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
+using namespace std;
 
-void ordenar(int *ini, int *mid)
-{
-    int *fin = mid + (mid - ini);
-    while (ini < mid && mid < fin)
-    {
-        
+void orden(int* ini, int* mid) {
+    int* fin = mid + (mid - ini - 1);
+    while (ini < mid && mid < fin) {
+        if (*ini > *mid) {
+            swap(*ini, *mid);
+        }
+        ini++;
+        for (int* temp = mid; temp < fin; temp++) {
+            if (*temp < *ini) {
+                swap(*temp, *ini);
+                if (*ini == *mid)
+                    mid++;
+            }
+        }
     }
-    
 }
 
-int main()
-{
-    int A[] = {0,2,4,6,8,1,3,5,7,9};
+int main() {
+    int A[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8 , 10 };
+    int* ini = A;
+    orden(A, A + 5);
 
+    for (int* p = A; p < A + 10; p++) {
+        cout << *p << " ";
+    }
+
+    return 0;
 }
