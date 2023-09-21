51dfa6139268b928dcee5748874e50d569d21968
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-09-20 21:13:56 -0500

    transp.cpp

--- Semana 4/transp.cpp
+++ Semana 4/transp.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void transp (int (*p)[4]    )
-{
-    for(int (*i) = *p; i < *(p + 1); ++i){
-        for (int (*j) = i; j < i; ++j){
-            int *ptr1 = (*(p + i) + j);
-            int *ptr2 =
+void transp(int (*p)[4]) {
+    for (int* i = *p; i < *p + 4; ++i) {
+        for (int* j = i + 1; j < *p + 4; ++j) {
+            // Swap the elements at (i, j) and (j, i) using pointers
+            int temp = *(*(p + (i - *p)) + (j - *p));
+            *(*(p + (i - *p)) + (j - *p)) = *(*(p + (j - *p)) + (i - *p));
+            *(*(p + (j - *p)) + (i - *p)) = temp;
         }
     }
-};
+}
 
-int main()
-{
-    int A[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+int main() {
+    int A[4][4] = {{1, 2, 3, 4},
+                  {5, 6, 7, 8},
+                  {9, 10, 11, 12},
+                  {13, 14, 15, 16}};
 
+    transp(A);
 
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
