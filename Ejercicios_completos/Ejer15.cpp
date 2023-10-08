3f942a602e45b7911ee1d7ea54fcc29c93a68dde
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-10-07 22:01:22 -0500

    Ejemplo Fibonacci con puntero a Funcion y arrays

---
+++ Ejercicios_completos/Ejer15.cpp
@@ -0,0 +1,29 @@
+//Ejemplo Fibonacci con puntero a Funcion y arrays 
+
+#include <iostream>
+using namespace std;
+
+int suma(int a, int b)
+{ 
+  return a+b;
+}
+
+void operar(int *p, int(*pr)(int,int))
+{
+  int tmp;
+  tmp = pr(*p,*(p+1));
+  *p = *(p+1);
+  *(p+1)=tmp;
+}
+
+int main() {
+
+  int a[2] = {0,1};
+
+  for(int i = 1;i<10;i++)
+  {
+    cout<<*a<<" ";
+    operar(a,suma);
+  }
+
+}
