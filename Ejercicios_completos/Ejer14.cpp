240d36a11d03b782e2784d1a2fc01a37d3f3791f
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-10-07 21:59:52 -0500

    Puntero Funcion Buuble Sort

---
+++ Ejercicios_completos/Ejer14.cpp
@@ -0,0 +1,56 @@
+//Puntero Funcion Buuble Sort
+
+// C++ program for implementation of Bubble sort 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+bool asc(int a, int b)
+{ return a > b; }
+
+bool desc(int a, int b)
+{ return a < b; }
+
+void swap(int *xp, int *yp) 
+{ 
+  int temp = *xp; 
+  *xp = *yp; 
+  *yp = temp; 
+} 
+
+// A function to implement bubble sort 
+void bubbleSort(int arr[], int n, bool (*comp)(int,int)) 
+{ 
+  int i, j; 
+  for (i = 0; i < n-1; i++)  
+  
+  // Last i elements are already in place 
+  for (j = 0; j < n-i-1; j++) 
+    if (comp(arr[j] , arr[j+1])) 
+      swap(&arr[j], &arr[j+1]); 
+} 
+
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+  int i; 
+  for (i = 0; i < size; i++) 
+    cout << arr[i] << " "; 
+  cout << endl; 
+} 
+
+// Driver code 
+int main() 
+{ 
+  int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+  int n = sizeof(arr)/sizeof(arr[0]); 
+  bubbleSort(arr, n,asc); 
+  cout<<"Sorted array: \n"; 
+  printArray(arr, n); 
+
+  bubbleSort(arr, n,desc); 
+  cout<<"Sorted array: \n"; 
+  printArray(arr, n); 
+  return 0; 
+} 
+
+// This code is contributed by rathbhupendra 
