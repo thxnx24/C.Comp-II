205fdd0fdb4655b3ad8c1a797502a23d3f698441
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-10-12 03:10:06 -0500

    Preg 8 Examen practica

---
+++ Practicas_examenes/Preg8.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+void fibonacci(long *ArrEnt, int ArrTam) {
+    if (ArrTam <= 2) {
+        return;  // No se pueden generar más números si ya tienes los primeros dos.
+    }
+
+    long *ptr = ArrEnt + 2;  // Apuntar al tercer elemento del array.
+
+    // Generamos el resto de la sucesión utilizando aritmética de punteros.
+    for (int i = 2; i < ArrTam; i++) {
+        long next = *(ptr - 2) + *(ptr - 1);
+        *ptr = next;
+        ptr++;
+    }
+}
+
+int main() {
+    int n;
+    std::cout << "Ingrese el número de términos de la sucesión de Fibonacci: ";
+    std::cin >> n;
+
+    if (n < 2) {
+        std::cout << "Se necesitan al menos 2 términos para generar la sucesión." << std::endl;
+        return 1;
+    }
+
+    long ArrEnt[n]; 
+    ArrEnt[0] = 0;
+    ArrEnt[1] = 1;
+
+    fibonacci(ArrEnt, n);  // Llamada a la función para generar el resto de la sucesión.
+
+    // Imprimir la sucesión de Fibonacci.
+    for (int i = 0; i < n; i++) {
+        std::cout << ArrEnt[i] << " ";
+    }
+
+    return 0;
+}
