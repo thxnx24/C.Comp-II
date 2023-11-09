5eade8e666e7beff62a6d6285ba96c08eeb9eba6
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2023-11-09 17:18:18 -0500

    Tarea Funtores asc - des

--- Semana 9/tarea_funtores.cpp
+++ Semana 9/tarea_funtores.cpp
@@ -1,26 +1,100 @@
 //Crea dos funtores que cuando creen una lista enlazada se ordene de forma ascendente o descendente
-
 #include <iostream>
 using namespace std;
 
-//LE <int> lista;
-
-template <class T>
-struct asc
-{
-    
+// Functor para ordenar de manera ascendente
+struct asc {
+    bool operator()(int a, int b) const {
+        return a < b;
+    }
 };
 
-template <class T>
-struct des
-{
-
+// Functor para ordenar de manera descendente
+struct des {
+    bool operator()(int a, int b) const {
+        return a > b;
+    }
 };
 
+template <typename T, typename ordenar_funtor>
+class lista_ordenada {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        explicit Node(const T& value) : data(value), next(nullptr) {}
+    };
 
+    Node* head;
+    ordenar_funtor comparador;
 
-int main()
-{
+public:
+    lista_ordenada() : head(nullptr) {}
+
+    ~lista_ordenada() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void insertar(const T& value) {
+        Node* newNode = new Node(value);
+
+        if (head == nullptr || comparador(value, head->data)) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr && comparador(current->next->data, value)) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
+
+    void imprime() const {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << std::endl;
+    }
+};
+
+int main() {
+    lista_ordenada<int, asc> lista_asc;
+    lista_asc.insertar(5);
+    lista_asc.insertar(2);
+    lista_asc.insertar(8);
+    lista_asc.insertar(1);
+    lista_asc.insertar(3);
+    lista_asc.insertar(12);
+    lista_asc.insertar(34);
+    lista_asc.insertar(-3);
+    lista_asc.insertar(4);
+    lista_asc.insertar(7);
     
+    cout << "Lista ordenada de manera ascendente: ";
+    lista_asc.imprime();
+
+    lista_ordenada<int, des> lista_des;
+    lista_des.insertar(5);
+    lista_des.insertar(2);
+    lista_des.insertar(8);
+    lista_des.insertar(1);
+    lista_des.insertar(3);
+    lista_des.insertar(12);
+    lista_des.insertar(34);
+    lista_des.insertar(-3);
+    lista_des.insertar(4);
+    lista_des.insertar(7);
+
+    cout << "Lista ordenada de manera descendente: ";
+    lista_des.imprime();
+
     return 0;
 }
